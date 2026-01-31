0e4f44afcd0ba8d76c9c9dec1f6f623a9f45da36
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2026-01-31 11:39:55 +0530

    Refactor timer functionality to include pause/resume feature and update time display

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,67 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const [timeLeft, setTimeLeft] = useState<String | null>(null);
+  // const [timeLeft, setTimeLeft] = useState<String | null | Number>(null);
+  const [totalTime, setTotalTime] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
+  const intervalRef = useRef(null);
 
   const reset = () => {
     setMinutes(0);
     setSeconds(0);
+    // setTimeLeft(0);
+    setTotalTime(0);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
   };
 
-  const startTimer = () => {
-    const timel = `${minutes} : ${seconds}`;
-    // const timel = minutes + seconds;
-    setTimeLeft(timel);
+  const pauseResume = () => {
+    if (totalTime > 0) {
+      setIsPaused((prev) => !prev);
+    }
   };
+  const startTimer = () => {
+    //const timel = `${minutes} : ${seconds}`;
+    // const timel = minutes + seconds;
+    const totalTime = minutes * 60 + seconds;
+    // setTimeout(() => {
+    //   console.log("run after 2 sec");
+    // }, 2000);
+
+    setTotalTime(totalTime);
+  };
+
+  useEffect(() => {
+    if (totalTime > 0 && !isPaused) {
+      intervalRef.current = setInterval(() => {
+        setTotalTime((prev) => {
+          const newValue = prev - 1;
+
+          if (newValue <= 0) {
+            if (intervalRef.current) {
+              clearInterval(intervalRef.current);
+              intervalRef.current = null;
+            }
+            return 0;
+          }
+          return newValue;
+        });
+      }, 1000);
+    } else if (intervalRef.current) {
+      intervalRef.current = null;
+    }
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    };
+  }, [totalTime, isPaused]);
 
   //  MM:SS
   return (
@@ -40,13 +86,13 @@ function App() {
         </div>
         <div>
           <button onClick={startTimer}>start</button>
-          <button>Pause</button>
+          <button onClick={pauseResume}>Pause/Resume</button>
           <button onClick={reset}>Reset</button>
         </div>
       </div>
       <div>
         <span>
-          <strong>Time</strong> {timeLeft}
+          <strong>Time</strong> {totalTime}
         </span>
       </div>
     </>
