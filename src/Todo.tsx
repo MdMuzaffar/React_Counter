e3b533409e5173253e7a2cc0dc2ac9559a3436e2
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2026-02-23 22:18:58 +0530

    Chor: Practice_React_todo_app

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -36,38 +36,101 @@ import { useState } from "react";
 //   );
 // }
 
+// export function todoList() {
+//   const [todos, setTodos] = useState<String[]>(["Milk", "Bread", "Eggs"]);
+//   const [items, setItems] = useState("");
+//   const addItems = () => {
+//     if (items.trim() !== "") {
+//       setTodos([...todos, items]);
+//       setItems("");
+//     }
+//   };
+//   const deleteItem = (index: number) => {
+//     setTodos(todos.filter((_, i) => i !== index));
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <input
+//           type="text"
+//           value={items}
+//           onChange={(e) => {
+//             setItems(e.target.value);
+//           }}
+//           placeholder="Add items...."
+//         />
+//         <button onClick={addItems}>Add Item</button>
+//       </div>
+//       <ul>
+//         <li>
+//           {todos.map((item, index) => {
+//             return (
+//               <li key={index}>
+//                 {item}
+//                 <button onClick={() => deleteItem(index)}>Delete</button>
+//               </li>
+//             );
+//           })}
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+
 export function todoList() {
-  const [todos, setTodos] = useState<String[]>(["Milk", "Bread", "Eggs"]);
-  const [items, setItems] = useState("");
-  const addItems = () => {
-    if (items.trim() !== "") {
-      setTodos([...todos, items]);
-      setItems("");
+  const [items, setItems] = useState<String[]>(["Milk", "Bread", "Jam"]);
+  const [input, setInput] = useState("");
+  const [edit, setEdit] = useState<number | null>(null);
+  const addItem = () => {
+    if (input.trim() === "") return;
+
+    if (edit !== null) {
+      //Update new item
+      const newItems = [...items];
+      newItems[edit] = input;
+      setItems(newItems);
+      setEdit(null);
+    } else {
+      //Add new item
+
+      setItems([...items, input]);
     }
+    setInput("");
   };
   const deleteItem = (index: number) => {
-    setTodos(todos.filter((_, i) => i !== index));
+    setItems(items.filter((_, i) => i !== index));
   };
 
+  const editItem = (index: number) => {
+    setInput(items[index] as string);
+    setEdit(index);
+  };
   return (
-    <div>
+    <>
       <div>
         <input
           type="text"
-          value={items}
+          value={input}
           onChange={(e) => {
-            e.target.value;
+            setInput(e.target.value);
           }}
-          placeholder="Add items...."
         />
-        <button onClick={addItems}></button>
+        <button onClick={addItem}>Add Item</button>
       </div>
       <ul>
-        {todos.map((item, index) => {
-          return <li key={index}>{item}</li>;
-        })}
-        <button onClick={deleteItem}>Delete</button>
+        <li>
+          {items.map((item, index) => {
+            return (
+              <li key={index}>
+                {item}
+                <button onClick={() => editItem(index)}> Edit</button>
+                <button onClick={() => deleteItem(index)}> Delete</button>
+              </li>
+            );
+          })}
+        </li>
       </ul>
-    </div>
+    </>
   );
 }
